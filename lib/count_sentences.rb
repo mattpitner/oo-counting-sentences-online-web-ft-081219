a5b00fa426a4ec8df537cd7131c41122bf5caf2c
Matt Pitner <mattpitner09@gmail.com>
2019-08-21 22:04:53 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
-
+  
+  
   def count_sentences
+    self.split(/[?!.] /).count 
+  end 
+  
 
-  end
-end
+  # def count_sentences
+  # new_setence = sentence.split(/[?!.] /)
+  # number = new_setence.count 
+  # if number = 0 
+  #   return 0
+  # end 
+  # end
+ 
+
+
+end 
