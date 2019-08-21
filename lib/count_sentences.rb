8f32fe309127bc09e0032336058b3a5f2c02e719
Matt Pitner <mattpitner09@gmail.com>
2019-08-21 21:36:30 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,6 +17,12 @@ class String
   def count_sentences(sentence)
    new_setence = sentence.split(/[?!.] /)
    number = new_setence.count 
-   return number 
   end
-end
+end   
+
+
+def count_sentences(sentence)
+  sentence.each do |new_sentence|
+    new_sentence.split(/[?!.] /)
+  end 
+end 
